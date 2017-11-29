3bc3b70f2b711f8ca0dab86604aab879dc8ef517
Diggory Hardy <git@dhardy.name>
2017-11-29 14:48:13 +0000

    k12: do some optimisations
    This brings performance close to that of the Rust Keccak library, but still worse.

--- src/k12.rs
+++ src/k12.rs
@@ -1,7 +1,7 @@
 // Implementation of K12, based on the reference implementation:
 // https://github.com/gvanas/KeccakCodePackage/blob/master/Standalone/KangarooTwelve-reference/K12.py
 //
-// This is a straight port from Python and not well optimised.
+// Some optimisations copied from https://github.com/RustCrypto/hashes/tree/master/sha3/src
 //
 // To the extent possible under law, the implementer has waived all copyright
 // and related or neighboring rights to the source code in this file.
@@ -30,68 +30,102 @@ macro_rules! array_ref {
     }}
 }
 
-fn KeccakP1600onLanes(lanes: &mut [[u64; 5]; 5], nrRounds: u32) {
-    fn ROL64(a: u64, n: u64) -> u64 {
-        let n64 = n % 64;
-        (a >> (64 - n64)) + (a << n64)
-    }
+macro_rules! REPEAT4 {
+    ($e: expr) => ( $e; $e; $e; $e; )
+}
 
-    let mut R = 1u64;
-    for round in 0..24 {
-        if round + nrRounds >= 24 {
-            // θ
-            let mut C = [0u64; 5];
-            for x in 0..5 {
-                C[x] = lanes[x].iter().fold(0, |a, b| a ^ b);
-            }
-            
-            let mut D = [0u64; 5];
-            for x in 0..5 {
-                D[x] = C[(x+4)%5] ^ ROL64(C[(x+1)%5], 1);
-            }
-            
-            for x in 0..5 {
-                for y in 0..5 {
-                    lanes[x][y] ^= D[x];
-                }
-            }
-            
-            // ρ and π
-            let (mut x, mut y) = (1, 0);
-            let mut current = lanes[x][y];
-            for t in 0..24 {
-                let x0 = x;
-                x = y;
-                y = (2*x0 + 3*y) % 5;
-                
-                let current0 = current;
-                current = lanes[x][y];
-                lanes[x][y] = ROL64(current0, (t+1)*(t+2)/2);
-            }
-            
-            // χ
-            for y in 0..5 {
-                let mut T = [0u64; 5];
-                for x in 0..5 {
-                    T[x] = lanes[x][y];
-                }
-                for x in 0..5 {
-                    lanes[x][y] = T[x] ^((!T[(x+1) % 5]) & T[(x+2)%5]);
-                }
-            }
-            
-            // ι
-            for j in 0..7 {
-                R = ((R << 1) ^ ((R >> 7)*0x71)) % 256;
-                if R & 2 != 0 {
-                    lanes[0][0] ^= 1 << ((1<<j)-1);
-                }
-            }
-        } else {
-            for _ in 0..7 {
-                R = ((R << 1) ^ ((R >> 7)*0x71)) % 256;
-            }
-        }
+macro_rules! REPEAT5 {
+    ($e: expr) => ( $e; $e; $e; $e; $e; )
+}
+
+macro_rules! REPEAT6 {
+    ($e: expr) => ( $e; $e; $e; $e; $e; $e; )
+}
+
+macro_rules! REPEAT24 {
+    ($e: expr, $s: expr) => (
+        REPEAT6!({ $e; $s; });
+        REPEAT6!({ $e; $s; });
+        REPEAT6!({ $e; $s; });
+        REPEAT5!({ $e; $s; });
+        $e;
+    )
+}
+
+macro_rules! FOR5 {
+    ($v: expr, $s: expr, $e: expr) => {
+        $v = 0;
+        REPEAT4!({
+            $e;
+            $v += $s;
+        });
+        $e;
+    }
+}
+pub const RC: [u64; 12] = [
+    0x000000008000808b,
+    0x800000000000008b,
+    0x8000000000008089,
+    0x8000000000008003,
+    0x8000000000008002,
+    0x8000000000000080,
+    0x000000000000800a,
+    0x800000008000000a,
+    0x8000000080008081,
+    0x8000000000008080,
+    0x0000000080000001,
+    0x8000000080008008,
+];
+
+// (0..24).map(|t| ((t+1)*(t+2)/2) % 64)
+pub const RHO: [u32; 24] = [
+    1, 3, 6, 10, 15, 21,28, 36, 45, 55, 2, 14, 27,
+    41, 56, 8, 25, 43, 62, 18, 39, 61, 20, 44
+];
+pub const PI: [usize; 24] = [
+    10, 7, 11, 17, 18, 3, 5, 16, 8, 21, 24, 4, 15, 23,
+    19, 13, 12, 2, 20, 14, 22, 9, 6, 1
+];
+
+fn KeccakP1600onLanes(lanes: &mut [u64; 25]) {
+    let mut C = [0u64; 5];
+    let (mut x, mut y): (usize, usize);
+    
+    for round in 0..12 {
+        // θ
+        FOR5!(x, 1, {
+            C[x] = lanes[x] ^ lanes[x+5] ^ lanes[x+10] ^ lanes[x+15] ^ lanes[x+20];
+        });
+        
+        FOR5!(x, 1, {
+            FOR5!(y, 5, {
+                lanes[x + y] ^= C[(x+4)%5] ^ C[(x+1)%5].rotate_left(1);
+            });
+        });
+        
+        // ρ and π
+        let mut a = lanes[1];
+        x = 0;
+        REPEAT24!({
+            C[0] = lanes[PI[x]];
+            lanes[PI[x]] = a.rotate_left(RHO[x]);
+        }, {
+            a = C[0];
+            x += 1;
+        });
+        
+        // χ
+        FOR5!(y, 5, {
+            FOR5!(x, 1, {
+                C[x] = lanes[x + y];
+            });
+            FOR5!(x, 1, {
+                lanes[x + y] = C[x] ^((!C[(x+1) % 5]) & C[(x+2)%5]);
+            });
+        });
+        
+        // ι
+        lanes[0] ^= RC[round];
     }
 }
 
@@ -102,18 +136,18 @@ fn write_u64(val: u64) -> [u8; 8] {
     unsafe{ *(&val.to_le() as *const u64 as *const [u8; 8]) }
 }
 
-fn KeccakP1600(state: &mut [u8; 200], nrRounds: u32) {
-    let mut lanes = [[0u64; 5]; 5];
+fn KeccakP1600(state: &mut [u8; 200]) {
+    let mut lanes = [0u64; 25];
     for x in 0..5 {
         for y in 0..5 {
-            lanes[x][y] = read_u64(array_ref!(state, 8*(x+5*y), 8));
+            lanes[x + 5*y] = read_u64(array_ref!(state, 8*(x+5*y), 8));
         }
     }
-    KeccakP1600onLanes(&mut lanes, nrRounds);
+    KeccakP1600onLanes(&mut lanes);
     for x in 0..5 {
         for y in 0..5 {
             let i = 8*(x+5*y);  // TODO index
-            state[i..i+8].copy_from_slice(&write_u64(lanes[x][y]));
+            state[i..i+8].copy_from_slice(&write_u64(lanes[x + 5*y]));
         }
     }
 }
@@ -133,7 +167,7 @@ fn F(inputBytes: &[u8], delimitedSuffix: u8, mut outputByteLen: usize) -> Vec<u8
         }
         inputOffset += blockSize;
         if blockSize == rateInBytes {
-            KeccakP1600(&mut state, 12);
+            KeccakP1600(&mut state);
             blockSize = 0;
         }
     }
@@ -141,10 +175,10 @@ fn F(inputBytes: &[u8], delimitedSuffix: u8, mut outputByteLen: usize) -> Vec<u8
     // === Do the padding and switch to the squeezing phase ===
     state[blockSize] ^= delimitedSuffix;
     if ((delimitedSuffix & 0x80) != 0) && (blockSize == (rateInBytes-1)) {
-        KeccakP1600(&mut state, 12);
+        KeccakP1600(&mut state);
     }
     state[rateInBytes-1] ^= 0x80;
-    KeccakP1600(&mut state, 12);
+    KeccakP1600(&mut state);
     
     // === Squeeze out all the output blocks ===
     let mut outputBytes = Vec::with_capacity(outputByteLen);
@@ -153,7 +187,7 @@ fn F(inputBytes: &[u8], delimitedSuffix: u8, mut outputByteLen: usize) -> Vec<u8
         outputBytes.extend_from_slice(&state[0..blockSize]);
         outputByteLen -= blockSize;
         if outputByteLen > 0 {
-            KeccakP1600(&mut state, 12);
+            KeccakP1600(&mut state);
         }
     }
     outputBytes
